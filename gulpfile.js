7e6133d4ea3c35c05ce21e0bce6f4249f78e7942
Derpet1 <aspancuk39@gmail.com>
2026-04-30 13:02:18 +0300

    added file copying task

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// function myTask(callback) {
+//   console.log('Hello Павло!');
+//   callback();
+// }
+
+// async function myTask2() {
+//   console.log('Task2: Hello Павло!');
+// }
+
+// exports.myTask = myTask;
+// exports.myTask2 = myTask2;
+
+// exports.myTask3 = async function myTask3() {
+//   console.log('Task3: Hello Павло!');
+// }
+
+// exports.myTask4 = async () => {
+//   console.log('Task4: Hello Павло!');
+// }
+const gulp = require('gulp');
+
+exports.cloneFiles = async () => {
+  return gulp.src('app/*.html')    
+    .pipe(gulp.dest('public'));   
+}
