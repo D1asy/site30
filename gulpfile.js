34a91a9e856f127a8a067c796fc85ef26b6b0d92
Derpet1 <aspancuk39@gmail.com>
2026-05-05 13:30:14 +0300

    completed minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,17 @@
-// function myTask(callback) {
-//   console.log('Hello Павло!');
-//   callback();
-// }
-
-// async function myTask2() {
-//   console.log('Task2: Hello Павло!');
-// }
-
-// exports.myTask = myTask;
-// exports.myTask2 = myTask2;
-
-// exports.myTask3 = async function myTask3() {
-//   console.log('Task3: Hello Павло!');
-// }
-
-// exports.myTask4 = async () => {
-//   console.log('Task4: Hello Павло!');
-// }
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
-exports.cloneFiles = async () => {
-  return gulp.src('app/**/*.*') 
+exports.minifyCSS = async () => {
+  return gulp.src('app/**/*.css')
+    .pipe(cleanCSS())
     .pipe(gulp.dest('public'));
 }
 
+exports.minifyJS = async () => {
+  return gulp.src('app/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public'));
+}
+
+exports.default = gulp.series(exports.minifyCSS, exports.minifyJS);
